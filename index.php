8a89be96ea4e0aa75b910a34ef5f42e531eedf1c
rootek-pl <wojciech.skrobek@gmail.com>
2016-06-16 11:39:16 +0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
 <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
+<?php
+$servername = "mysql.hostinger.pl";
+$username = "u693034799_test";
+$password = "zaq12wsx";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
 <header>Wojciech Skrobek</header>
 <div class="row">
   <div class="col-3 menu">
